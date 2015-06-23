a0554f369ecbe23d43a64b1b68a24d51d2e3057d
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-06-23 23:52:09 +0200

    Tested basic Unity functionality

--- src/utils/DIContainersKnowledgeBase/UnityKnowledgeBase/KnowledgeBase.cs
+++ src/utils/DIContainersKnowledgeBase/UnityKnowledgeBase/KnowledgeBase.cs
@@ -1,12 +1,47 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.Practices.Unity;
+using NUnit.Framework;
+using Shared.InterfaceImplementation;
+using FluentAssertions;
 
 namespace UnityKnowledgeBase
 {
     public class KnowledgeBase
     {
 		[Test]
-		public void destination_type_lifetime_applies_to_source_type()
+		public void default_lifetime_is_per_resolve()
 		{
+			// Arrange
+			var container = new UnityContainer();
+			container.RegisterType<IInterface, Implementation>();
+
+			// Act
+			var instance1 = container.Resolve<IInterface>();
+			var instance2 = container.Resolve<IInterface>();
+
+			// Assert
+			instance2.Should().NotBeSameAs(instance1);
+		}
+
+		[Test]
+		public void singleton_lifetime_is_available()
+		{
+			// Arrange
+			var container = new UnityContainer();
+			container.RegisterType<IInterface, Implementation>(new ContainerControlledLifetimeManager());
+
+			// Act
+			var instance1 = container.Resolve<IInterface>();
+			var instance2 = container.Resolve<IInterface>();
+
+			// Assert
+			instance2.ShouldBeEquivalentTo(instance1);
+		}
+
+		[Test]
+		public void implementation_lifetime_applies_to_interface()
+		{
+			var container = new UnityContainer();
+			container.RegisterType<IInterface, Implementation>();
 
 		}
     }
