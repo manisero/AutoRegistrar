5e92f180dfc25bfaf62c8b1d7edce14b498fb96f
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-06-23 23:15:02 +0200

    Started implementing BuildRegistrationMapCommand

---
+++ src/Manisero.AutoRegistrar/Manisero.AutoRegistrar.Commands/IBuildRegistrationMapCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Manisero.AutoRegistrar.Commands
+{
+	public class BuildRegistrationMapCommandParameter
+	{
+		
+	}
+
+	public interface IBuildRegistrationMapCommand : IReturningCommand<BuildRegistrationMapCommandParameter, IDictionary<Type, Type>>
+	{
+	}
+}
---
+++ src/Manisero.AutoRegistrar/Manisero.AutoRegistrar.Commands/_Impl/BuildRegistrationMapCommand.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Manisero.AutoRegistrar.Commands._Impl
+{
+	public class BuildRegistrationMapCommand : IBuildRegistrationMapCommand
+	{
+		public IDictionary<Type, Type> Execute(BuildRegistrationMapCommandParameter parameter)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
