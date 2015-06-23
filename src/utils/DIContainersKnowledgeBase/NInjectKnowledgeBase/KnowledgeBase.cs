d07e1f306a80250f9f1522747fc248093f61a0b0
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-06-24 00:06:21 +0200

    Started creating NInject knowledge base

--- src/utils/DIContainersKnowledgeBase/NInjectKnowledgeBase/KnowledgeBase.cs
+++ src/utils/DIContainersKnowledgeBase/NInjectKnowledgeBase/KnowledgeBase.cs
@@ -1,13 +1,80 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace NInjectKnowledgeBase
 {
     public class KnowledgeBase
     {
 		[Test]
-		public void destination_type_lifetime_applies_to_source_type()
+		public void default_lifetime_is_per_resolve()
 		{
-			
+			throw new NotImplementedException();
+
+			// Arrange
+
+			// Act
+
+			// Assert
+		}
+
+		[Test]
+		public void singleton_lifetime_is_available()
+		{
+			throw new NotImplementedException();
+
+			// Arrange
+
+			// Act
+
+			// Assert
+		}
+
+		[Test]
+		public void implementation_lifetime_applies_to_interface()
+		{
+			throw new NotImplementedException();
+
+			// Arrange
+
+			// Act
+
+			// Assert
+		}
+
+		[Test]
+		public void implementation_lifetime_applies_to_base_type()
+		{
+			throw new NotImplementedException();
+
+			// Arrange
+
+			// Act
+
+			// Assert
+		}
+
+		[Test]
+		public void interface_chaining_is_not_supported()
+		{
+			throw new NotImplementedException();
+
+			// Arrange
+
+			// Act
+
+			// Assert
+		}
+
+		[Test]
+		public void class_chaining_is_not_supported()
+		{
+			throw new NotImplementedException();
+
+			// Arrange
+
+			// Act
+
+			// Assert
 		}
     }
 }
