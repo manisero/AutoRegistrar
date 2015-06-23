da2b0e8ec72b0ef9bf729e0a246a02a8dece3021
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-06-24 00:14:39 +0200

    Created basic NInject knowledge base

--- src/utils/DIContainersKnowledgeBase/NInjectKnowledgeBase/KnowledgeBase.cs
+++ src/utils/DIContainersKnowledgeBase/NInjectKnowledgeBase/KnowledgeBase.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using FluentAssertions;
 using NUnit.Framework;
+using Ninject;
+using Shared.InterfaceImplementation;
 
 namespace NInjectKnowledgeBase
 {
@@ -8,33 +11,38 @@ namespace NInjectKnowledgeBase
 		[Test]
 		public void default_lifetime_is_per_resolve()
 		{
-			throw new NotImplementedException();
-
 			// Arrange
+			var kernel = new StandardKernel();
+			kernel.Bind<IInterface>().To<Implementation>();
 
 			// Act
+			var instance1 = kernel.Get<IInterface>();
+			var instance2 = kernel.Get<IInterface>();
 
 			// Assert
+			instance2.Should().NotBeSameAs(instance1);
 		}
 
 		[Test]
 		public void singleton_lifetime_is_available()
 		{
-			throw new NotImplementedException();
-
 			// Arrange
+			var kernel = new StandardKernel();
+			kernel.Bind<IInterface>().To<Implementation>().InSingletonScope();
 
 			// Act
+			var instance1 = kernel.Get<IInterface>();
+			var instance2 = kernel.Get<IInterface>();
 
 			// Assert
+			instance2.ShouldBeEquivalentTo(instance1);
 		}
 
 		[Test]
 		public void implementation_lifetime_applies_to_interface()
 		{
-			throw new NotImplementedException();
-
 			// Arrange
+			throw new NotImplementedException();
 
 			// Act
 
@@ -44,9 +52,8 @@ namespace NInjectKnowledgeBase
 		[Test]
 		public void implementation_lifetime_applies_to_base_type()
 		{
-			throw new NotImplementedException();
-
 			// Arrange
+			throw new NotImplementedException();
 
 			// Act
 
@@ -56,9 +63,8 @@ namespace NInjectKnowledgeBase
 		[Test]
 		public void interface_chaining_is_not_supported()
 		{
-			throw new NotImplementedException();
-
 			// Arrange
+			throw new NotImplementedException();
 
 			// Act
 
@@ -68,9 +74,8 @@ namespace NInjectKnowledgeBase
 		[Test]
 		public void class_chaining_is_not_supported()
 		{
-			throw new NotImplementedException();
-
 			// Arrange
+			throw new NotImplementedException();
 
 			// Act
 
