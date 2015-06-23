a2d5766087e83a6358ed31ad97175c262357f6cb
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-06-24 01:06:04 +0200

    Implementing registrar

--- src/Manisero.AutoRegistrar/Manisero.AutoRegistrar.Tests/Stubs/Registrar.cs
+++ src/Manisero.AutoRegistrar/Manisero.AutoRegistrar.Tests/Stubs/Registrar.cs
@@ -1,5 +1,9 @@
-﻿using Manisero.AutoRegistrar.Commands;
+﻿using System;
+using System.Collections.Generic;
+using Manisero.AutoRegistrar.Commands;
 using Manisero.AutoRegistrar.Commands._Impl;
+using Manisero.AutoRegistrar.Queries.Tests.Stubs;
+using Manisero.AutoRegistrar.Queries._Impl;
 
 namespace Manisero.AutoRegistrar.Tests.Stubs
 {
@@ -16,9 +20,27 @@ namespace Manisero.AutoRegistrar.Tests.Stubs
 				});
 
 			// 2. Build type map
+			var typeMap = new Dictionary<Type, Type>();
+
 			// 3. Get initial lifetime map
+			var lifetimeMap = new Dictionary<Type, int>();
+
 			// 4. Include concrete types from type map in lifetime map
+			var includeTypeInLifetimeMapCommand = new IncludeTypeInLifetimeMapCommand<int>(new TypeDependenciesQuery(),
+																						   new LongestIntLifetimeQuery(),
+																						   new IsIntLifetimeShorterThanQuery());
+
+			foreach (var destinationType in typeMap.Values)
+			{
+				includeTypeInLifetimeMapCommand.Execute(new IncludeTypeInLifetimeMapCommandParameter<int>
+					{
+						LifetimeMap = lifetimeMap,
+						Type = destinationType
+					});
+			}
+
 			// 5. Create registration map
+
 			// 6. Register
 		}
 	}
