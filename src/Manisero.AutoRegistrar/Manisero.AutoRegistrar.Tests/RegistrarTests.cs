5714411680485ab79f06b9239fb9faa46b9d74a3
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-06-24 01:07:18 +0200

    Created RegistrarTests

---
+++ src/Manisero.AutoRegistrar/Manisero.AutoRegistrar.Tests/RegistrarTests.cs
@@ -0,0 +1,20 @@
+﻿using Manisero.AutoRegistrar.Tests.Stubs;
+using NUnit.Framework;
+
+namespace Manisero.AutoRegistrar.Tests
+{
+	public class RegistrarTests
+	{
+		[Test]
+		public void test()
+		{
+			// Arrange
+			var registrar = new Registrar();
+
+			// Act
+			registrar.Register();
+
+			// Assert
+		}
+	}
+}
