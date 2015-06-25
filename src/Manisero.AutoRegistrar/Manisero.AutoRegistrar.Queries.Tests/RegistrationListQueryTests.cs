c3f6ad08b020fd61d4975e43dc9ec0e9d44770f7
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-06-25 23:54:07 +0200

    Improved RegistrationListQueryTests

--- src/Manisero.AutoRegistrar/Manisero.AutoRegistrar.Queries.Tests/RegistrationListQueryTests.cs
+++ src/Manisero.AutoRegistrar/Manisero.AutoRegistrar.Queries.Tests/RegistrationListQueryTests.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using FluentAssertions;
 using Manisero.AutoRegistrar.Core;
 using Manisero.AutoRegistrar.Queries._Impl;
-using Manisero.AutoRegistrar.Tests.Core.TestsHelpers.DependencyHelpers;
+using Manisero.AutoRegistrar.Tests.Core.TestsHelpers.Scenario.CodeBase;
+using Manisero.AutoRegistrar.Tests.Core.TestsHelpers.Scenario.CodeBase.Queries;
+using Manisero.AutoRegistrar.Tests.Core.TestsHelpers.Scenario.CodeBase.Queries._Impl;
 using NUnit.Framework;
 
 namespace Manisero.AutoRegistrar.Queries.Tests
@@ -26,23 +28,45 @@ namespace Manisero.AutoRegistrar.Queries.Tests
 		}
 		
 		[Test]
-		public void assigns_lifetimes_to_implementations()
+		public void assigns_lifetimes_to_implementations_and_ignores_lifetimes_of_types_not_present_in_type_map()
 		{
 			// Arrange & Act
 			var typeMap = new Dictionary<Type, Type>
 				{
-					{ typeof(IInterface), typeof(Implementation) }
+					{ typeof(IDataQuery), typeof(DataQuery) },
+					{ typeof(IPureQuery), typeof(PureQuery) },
 				};
 
 			var lifetimeMap = new Dictionary<Type, int>
 				{
-					{ typeof(Implementation), 3 }
+					{ typeof(DataQuery), 3 },
+					{ typeof(PureQuery), 5 },
+					{ typeof(GlobalState), 7 },
 				};
 
 			var result = Execute(typeMap, lifetimeMap);
 
 			// Assert
-			result.Should().Contain(x => x.SourceType == typeof(IInterface) && x.DestinationType == typeof(Implementation) && x.Lifetime == 3);
+			result.Should().HaveCount(2);
+			result.Should().Contain(x => x.SourceType == typeof(IDataQuery) && x.DestinationType == typeof(DataQuery) && x.Lifetime == 3);
+			result.Should().Contain(x => x.SourceType == typeof(IPureQuery) && x.DestinationType == typeof(PureQuery) && x.Lifetime == 5);
+		}
+
+		[Test]
+		public void implementation_not_present_in_lifetime_map___Exception()
+		{
+			// Arrange
+			var typeMap = new Dictionary<Type, Type>
+				{
+					{ typeof(IDataQuery), typeof(DataQuery) }
+				};
+
+			var lifetimeMap = new Dictionary<Type, int>();
+
+			Action act = () => Execute(typeMap, lifetimeMap);
+
+			// Assert
+			act.ShouldThrow<Exception>();
 		}
 	}
 }
