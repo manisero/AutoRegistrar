6b007550e4223c599cd0ba3cbb6038d88dce7582
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-06-26 00:06:35 +0200

    Used BuildRegistrationListCommand in Registrar

--- src/Manisero.AutoRegistrar/Manisero.AutoRegistrar.Tests/Stubs/Registrar.cs
+++ src/Manisero.AutoRegistrar/Manisero.AutoRegistrar.Tests/Stubs/Registrar.cs
@@ -6,19 +6,23 @@ using Manisero.AutoRegistrar.Commands._Impl;
 using Manisero.AutoRegistrar.Queries.Tests.Stubs.TestLifetimeLifetime;
 using Manisero.AutoRegistrar.Queries._Impl;
 using Manisero.AutoRegistrar.Tests.Core.TestsHelpers.Scenario;
-using System.Linq;
 
 namespace Manisero.AutoRegistrar.Tests.Stubs
 {
 	public class Registrar
 	{
-		public class Registration<TLifetime>
+		private readonly IBuildRegistrationListCommand<TestLifetime> _buildRegistrationListCommand;
+
+		public Registrar()
 		{
-			public Type SourceType { get; set; }
+			_buildRegistrationListCommand = new BuildRegistrationListCommand<TestLifetime>(new LoadAndRetrieveAvailableTypesCommand(),
+																						   new IncludeTypeInTypeMapCommand(),
+																						   new IncludeTypeInLifetimeMapCommand<TestLifetime>(new TypeDependenciesQuery(),
+																																			 new LongestTestLifetimeQuery(),
+																																			 new IsTypeConstructibleQuery(),
+																																			 new IsTestLifetimeShorterThanQuery()),
+																						   new RegistrationListQuery<TestLifetime>());
 
-			public Type DestinationType { get; set; }
-
-			public TLifetime Lifetime { get; set; }
 		}
 
 		public void Register(Assembly rootAssembly,
@@ -27,55 +31,16 @@ namespace Manisero.AutoRegistrar.Tests.Stubs
 							 IDictionary<Type, Type> typeMap,
 							 IDictionary<Type, TestLifetime> lifetimeMap)
 		{
-			// Get available types
-			var loadAndRetrieveAvailableTypesCommand = new LoadAndRetrieveAvailableTypesCommand();
-			var availableTypes = loadAndRetrieveAvailableTypesCommand.Execute(new LoadAndRetrieveAvailableTypesCommandParameter
+			var buildRegistrationListCommandParameter = new BuildRegistrationListCommandParameter<TestLifetime>
 				{
 					RootAssembly = rootAssembly,
 					ReferencedAssemblyFilter = referencedAssemblyFilter,
-					TypeFilter = typeFilter
-				});
+					TypeFilter = typeFilter,
+					TypeMap = typeMap,
+					LifetimeMap = lifetimeMap
+				};
 
-			// Build type map
-			var includeTypeInTypeMapCommand = new IncludeTypeInTypeMapCommand();
-
-			foreach (var type in availableTypes)
-			{
-				includeTypeInTypeMapCommand.Execute(new IncludeTypeInTypeMapCommandParameter
-					{
-						TypeMap = typeMap,
-						Type = type,
-						AvailableTypes = availableTypes
-					});
-			}
-
-			// Include implementations from Type Map in Lifetime Map
-			var includeTypeInLifetimeMapCommand = new IncludeTypeInLifetimeMapCommand<TestLifetime>(new TypeDependenciesQuery(),
-																									new LongestTestLifetimeQuery(),
-																									new IsTypeConstructibleQuery(),
-																									new IsTestLifetimeShorterThanQuery());
-
-			foreach (var implementationType in typeMap.Values)
-			{
-				if (!lifetimeMap.ContainsKey(implementationType))
-				{
-					includeTypeInLifetimeMapCommand.Execute(new IncludeTypeInLifetimeMapCommandParameter<TestLifetime>
-						{
-							LifetimeMap = lifetimeMap,
-							Type = implementationType,
-							TypeMap = typeMap
-						});
-				}
-			}
-
-			// Create registration map/list
-			var registrarions = typeMap.Select(x => new Registration<TestLifetime>
-				{
-					SourceType = x.Key,
-					DestinationType = x.Value,
-					Lifetime = lifetimeMap[x.Value]
-				})
-									   .ToList();
+			var registrations = _buildRegistrationListCommand.Execute(buildRegistrationListCommandParameter);
 
 			// Register
 		}
