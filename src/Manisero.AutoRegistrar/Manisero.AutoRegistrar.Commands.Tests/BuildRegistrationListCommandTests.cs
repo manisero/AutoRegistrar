ae75bd4f03d0d54b6b3bb6b77ab0f4ee1fe2eac8
Michal Aniserowicz <m.aniserowicz@funandmobile.com>
2015-06-25 23:58:33 +0200

    Added assertions to BuildRegistrationListCommandTests

--- src/Manisero.AutoRegistrar/Manisero.AutoRegistrar.Commands.Tests/BuildRegistrationListCommandTests.cs
+++ src/Manisero.AutoRegistrar/Manisero.AutoRegistrar.Commands.Tests/BuildRegistrationListCommandTests.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using FluentAssertions;
 using Manisero.AutoRegistrar.Commands._Impl;
 using Manisero.AutoRegistrar.Queries.Tests.Stubs.TestLifetimeLifetime;
 using Manisero.AutoRegistrar.Queries._Impl;
 using Manisero.AutoRegistrar.Tests.Core.TestsHelpers.Scenario;
 using Manisero.AutoRegistrar.Tests.Core.TestsHelpers.Scenario.CodeBase;
+using Manisero.AutoRegistrar.Tests.Core.TestsHelpers.Scenario.CodeBase.Commands;
+using Manisero.AutoRegistrar.Tests.Core.TestsHelpers.Scenario.CodeBase.Commands._Impl;
+using Manisero.AutoRegistrar.Tests.Core.TestsHelpers.Scenario.CodeBase.Queries;
+using Manisero.AutoRegistrar.Tests.Core.TestsHelpers.Scenario.CodeBase.Queries._Impl;
+using Manisero.AutoRegistrar.Tests.Core.TestsHelpers.Scenario.CodeBase._Impl;
 using NUnit.Framework;
 
 namespace Manisero.AutoRegistrar.Commands.Tests
@@ -37,6 +43,18 @@ namespace Manisero.AutoRegistrar.Commands.Tests
 			var result = command.Execute(parameter);
 
 			// Assert
+			result.Should().HaveCount(8);
+
+			result.Should().Contain(x => x.SourceType == typeof(IDataContext) && x.DestinationType == typeof(DataContext) && x.Lifetime == TestLifetime.Request);
+			result.Should().Contain(x => x.SourceType == typeof(IRandomBehavior) && x.DestinationType == typeof(RandomBehavior) && x.Lifetime == TestLifetime.Transient);
+
+			result.Should().Contain(x => x.SourceType == typeof(IDataQuery) && x.DestinationType == typeof(DataQuery) && x.Lifetime == TestLifetime.Request);
+			result.Should().Contain(x => x.SourceType == typeof(IPureQuery) && x.DestinationType == typeof(PureQuery) && x.Lifetime == TestLifetime.Sigleton);
+			result.Should().Contain(x => x.SourceType == typeof(IRandomQuery) && x.DestinationType == typeof(RandomQuery) && x.Lifetime == TestLifetime.Transient);
+
+			result.Should().Contain(x => x.SourceType == typeof(IDataCommand) && x.DestinationType == typeof(DataCommand) && x.Lifetime == TestLifetime.Request);
+			result.Should().Contain(x => x.SourceType == typeof(IGlobalStateCommand) && x.DestinationType == typeof(GlobalStateCommand) && x.Lifetime == TestLifetime.Sigleton);
+			result.Should().Contain(x => x.SourceType == typeof(IRandomDataCommand) && x.DestinationType == typeof(RandomDataCommand) && x.Lifetime == TestLifetime.Transient);
 		}
 	}
 }
