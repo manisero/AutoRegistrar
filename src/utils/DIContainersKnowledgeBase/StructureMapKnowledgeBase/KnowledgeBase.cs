9797c8fcc6370ac5647bbbc388ee307074f80520
manisero <michalaniserowicz@gmail.com>
2015-08-09 20:33:00 +0200

    Started creating StructureMap knowledge base

---
+++ src/utils/DIContainersKnowledgeBase/StructureMapKnowledgeBase/KnowledgeBase.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Shared.InterfaceImplementation;
+using StructureMap;
+
+namespace StructureMapKnowledgeBase
+{
+    public class KnowledgeBase
+    {
+        [Test]
+        public void default_lifetime_is_transient()
+        {
+            // Arrange
+            var container = new Container();
+            container.Configure(x => x.For<IInterface>().Use<Implementation>());
+
+            // Act
+            var instance1 = container.GetInstance<IInterface>();
+            var instance2 = container.GetInstance<IInterface>();
+
+            // Assert
+            instance2.Should().NotBeSameAs(instance1);
+        }
+
+        [Test]
+        public void singleton_lifetime_is_available()
+        {
+            // Arrange
+            var container = new Container();
+            container.Configure(x => x.For<IInterface>().Use<Implementation>().Singleton());
+
+            // Act
+            var instance1 = container.GetInstance<IInterface>();
+            var instance2 = container.GetInstance<IInterface>();
+
+            // Assert
+            instance2.ShouldBeEquivalentTo(instance1);
+        }
+    }
+}
