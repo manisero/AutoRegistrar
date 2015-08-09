a5e3401eb12690402db7ef65f8cb125ae6cf2c97
manisero <michalaniserowicz@gmail.com>
2015-08-09 21:27:55 +0200

    Added basic tests to Autofac knowledge base

--- src/utils/DIContainersKnowledgeBase/AutofacKnowledgeBase/KnowledgeBase.cs
+++ src/utils/DIContainersKnowledgeBase/AutofacKnowledgeBase/KnowledgeBase.cs
@@ -1,7 +1,44 @@
-﻿namespace AutofacKnowledgeBase
+﻿using Autofac;
+using FluentAssertions;
+using NUnit.Framework;
+using Shared.InterfaceImplementation;
+
+namespace AutofacKnowledgeBase
 {
     public class KnowledgeBase
     {
+        [Test]
+        public void default_lifetime_is_transient()
+        {
+            // Arrange
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Implementation>().As<IInterface>();
 
+            var container = builder.Build();
+
+            // Act
+            var instance1 = container.Resolve<IInterface>();
+            var instance2 = container.Resolve<IInterface>();
+
+            // Assert
+            instance2.Should().NotBeSameAs(instance1);
+        }
+
+        [Test]
+        public void singleton_lifetime_is_available()
+        {
+            // Arrange
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Implementation>().As<IInterface>().SingleInstance();
+
+            var container = builder.Build();
+
+            // Act
+            var instance1 = container.Resolve<IInterface>();
+            var instance2 = container.Resolve<IInterface>();
+
+            // Assert
+            instance2.ShouldBeEquivalentTo(instance1);
+        }
     }
 }
